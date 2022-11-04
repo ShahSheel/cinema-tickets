1b744a56d383f0c750eb7e8736d97d14e389f801
Sheel Shah <shahsheel@outlook.com>
2022-11-04 20:30:39 +0000

    Added basic runnable

--- src/main/java/uk/gov/dwp/uc/pairtest/Application.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/Application.java
@@ -1,17 +1,34 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.validation.TicketServiceValidator;
 
 public class Application {
 
     public static void main(String[] args) {
 
+        TicketPaymentService ticketPaymentService = new TicketPaymentService() {
+            @Override
+            public void makePayment(long accountId, int totalAmountToPay) {
 
-        TicketServiceImpl ticketServiceImpl = new TicketServiceImpl();
-        TicketTypeRequest ticketTypeRequest = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
-        TicketTypeRequest ticketTypeRequest2 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
-        TicketTypeRequest ticketTypeRequest3 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
+            }
+        };
+        SeatReservationService seatReservationService = new SeatReservationService() {
+            @Override
+            public void reserveSeat(long accountId, int totalSeatsToAllocate) {
 
-        ticketServiceImpl.purchaseTickets(12345L, ticketTypeRequest, ticketTypeRequest2);
+            }
+        };
+
+
+        TicketServiceImpl ticketServiceImpl = new TicketServiceImpl(ticketPaymentService,seatReservationService);
+        TicketTypeRequest adult = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1);
+        TicketTypeRequest child = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1);
+        TicketTypeRequest infant = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1);
+
+
+        ticketServiceImpl.purchaseTickets(12345L, adult, child, infant);
     }
 }
